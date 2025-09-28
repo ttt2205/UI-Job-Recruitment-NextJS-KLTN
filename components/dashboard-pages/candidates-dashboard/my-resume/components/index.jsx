8b7ed6490848d32771835c0c0e5cbd75b269ccd7
ttt2205 <thanhtrung22052004@gmail.com>
2025-09-28 14:44:56 +0700

    refactor(candidate-dashboard): restructure resume UI components

--- components/dashboard-pages/candidates-dashboard/my-resume/components/index.jsx
+++ components/dashboard-pages/candidates-dashboard/my-resume/components/index.jsx
@@ -48,29 +48,10 @@ const index = () => {
     }
   };
 
-  // <!-------------------- Handler Functions -------------------->
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
-
   // <!-------------------- Render UI -------------------->
   return (
-    <form className="default-form" onClick={handleSubmit}>
+    <form className="default-form">
       <div className="row">
-        <div className="form-group col-lg-6 col-md-12">
-          <label>Select Your CV</label>
-          <select className="chosen-single form-select">
-            <option>My CV</option>
-          </select>
-        </div>
-        {/* <!-- Input --> */}
-
-        <div className="form-group col-lg-12 col-md-12">
-          <label>Description</label>
-          <textarea placeholder="Spent several years working on sheep on Wall Street. Had moderate success investing in Yugo's on Wall Street. Managed a small team buying and selling Pogo sticks for farmers. Spent several years licensing licorice in West Palm Beach, FL. Developed several new methods for working it banjos in the aftermarket. Spent a weekend importing banjos in West Palm Beach, FL.In this position, the Software Engineer collaborates with Evention's Development team to continuously enhance our current software solutions as well as create new solutions to eliminate the back-office operations and management challenges present"></textarea>
-        </div>
-        {/* <!-- About Company --> */}
-
         <div className="form-group col-lg-12 col-md-12">
           <Education data={educationList} />
           {/* <!-- Resume / Education --> */}
@@ -90,19 +71,6 @@ const index = () => {
           <Awards data={awardsList} />
         </div>
         {/* <!-- End Award --> */}
-
-        {/* <div className="form-group col-lg-6 col-md-12">
-          <label>Skills </label>
-          <SkillsMultiple />
-        </div> */}
-        {/* <!-- Multi Selectbox --> */}
-
-        <div className="form-group col-lg-12 col-md-12">
-          <button type="submit" className="theme-btn btn-style-one">
-            Save
-          </button>
-        </div>
-        {/* <!-- Input --> */}
       </div>
       {/* End .row */}
     </form>
--- components/dashboard-pages/candidates-dashboard/my-resume/index.jsx
+++ components/dashboard-pages/candidates-dashboard/my-resume/index.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import MobileMenu from "../../../header/MobileMenu";
 import LoginPopup from "../../../common/form/login/LoginPopup";
 import DashboardCandidatesSidebar from "../../../header/DashboardCandidatesSidebar";
@@ -8,6 +10,12 @@ import DashboardCandidatesHeader from "../../../header/DashboardCandidatesHeader
 import MenuToggler from "../../MenuToggler";
 
 const index = () => {
+  // <!-------------------- Handler Functions -------------------->
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  // <!-------------------- Render UI -------------------->
   return (
     <div className="page-wrapper dashboard">
       <span className="header-span"></span>
@@ -40,6 +48,51 @@ const index = () => {
                 <div className="tabs-box">
                   <div className="widget-title">
                     <h4>My Profile</h4>
+                  </div>
+                  {/* End widget-title */}
+
+                  <div className="widget-content">
+                    <form className="default-form" onClick={handleSubmit}>
+                      <div className="row">
+                        <div className="form-group col-lg-6 col-md-12">
+                          <label>Select Your CV</label>
+                          <select className="chosen-single form-select">
+                            <option>My CV</option>
+                          </select>
+                        </div>
+                        {/* <!-- Input --> */}
+
+                        <div className="form-group col-lg-12 col-md-12">
+                          <label>Description</label>
+                          <textarea placeholder="Introduce yourself to the employer"></textarea>
+                        </div>
+                        {/* <!-- About Candidate --> */}
+
+                        <div className="form-group col-lg-12 col-md-12">
+                          <button
+                            type="submit"
+                            className="theme-btn btn-style-one"
+                          >
+                            Save
+                          </button>
+                        </div>
+                        {/* <!-- Input --> */}
+                      </div>
+                    </form>
+                  </div>
+                  {/* End widget-content */}
+                </div>
+              </div>
+              {/* End ls-widget */}
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="ls-widget">
+                <div className="tabs-box">
+                  <div className="widget-title">
+                    <h4>Your Qualifications</h4>
                   </div>
                   {/* End widget-title */}
 
